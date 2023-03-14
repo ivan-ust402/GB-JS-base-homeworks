47bdf79c15800425b88d62b6f6d7e67766660443
Ivan Ustyantsev <vangold915@gmail.com>
2023-03-14 17:15:07 +0700

    performStep method filled

--- additions/lesson7/additional/snakeGame/src/Snake.js
+++ additions/lesson7/additional/snakeGame/src/Snake.js
@@ -19,4 +19,32 @@ class Snake {
 
         this.direction = 'down';
     }
+
+    /**
+     * Метод осуществляет шаг змейки. Добавляет ячейку перед существующим
+     * положением головы и удаляет одну ячейку в хвосте.
+     */
+    performStep() {
+        let currentHeadCoords = this.body[0];
+        let newHeadCoords = {
+            x: currentHeadCoords.x,
+            y: currentHeadCoords.y,
+        };
+        switch (this.direction) {
+            case 'down': 
+                newHeadCoords.y++;
+                break;
+            case 'up':
+                newHeadCoords.y--;
+                break;
+            case 'left':
+                newHeadCoords.x--;
+                break;
+            case 'right':
+                newHeadCoords.x++;
+                break;
+        }
+        this.body.unshift(newHeadCoords);
+        this.body.pop();
+    }
 }
