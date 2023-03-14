a14ce4c895e2d104c880d192ec81557252c7e9c8
Ivan Ustyantsev <vangold915@gmail.com>
2023-03-14 13:55:00 +0700

    Created Game.js file

---
+++ additions/lesson7/additional/snakeGame/src/Game.js
@@ -0,0 +1,7 @@
+// Главный объект, где будет происходить все движение
+class Game {
+    constructor() {
+        this.tickIdentifier = null;
+        this.messageEl = document.getElementById('message');
+    }
+}
--- additions/lesson7/additional/snakeGame/src/Main.js
+++ additions/lesson7/additional/snakeGame/src/Main.js
@@ -9,11 +9,13 @@ window.addEventListener('load', () => {
     const board = new Board();
 
     const food = new Food();
+    const game = new Game();
 
     // Настройки
     settings.init({speed: 5, winLength: 5});
     board.init(settings, snake);
     food.init(settings, snake, board);
+    game.init(settings, status, board, snake, menu, food);
 
     // Рисуем игровое поле
     board.renderBoard();
